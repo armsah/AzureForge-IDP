3af152fac5e320450166268c08a74088a06fa521
Armen Sahakyan <sahakyanarmen5@gmail.com>
2026-09-01 18:26:28 +0300

    fix: use immutable GitHub OIDC subject

--- infra/bootstrap/platform/locals.tf
+++ infra/bootstrap/platform/locals.tf
@@ -7,5 +7,5 @@ locals {
     criticality = "high"
   }
 
-  github_subject = "repo:${var.github_owner}/${var.github_repository}:ref:refs/heads/${var.github_branch}"
+  github_subject = "repo:${var.github_owner}@8946419/${var.github_repository}@1349877986:ref:refs/heads/${var.github_branch}"
 }
